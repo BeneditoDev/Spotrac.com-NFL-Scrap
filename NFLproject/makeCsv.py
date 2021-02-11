0ed79113c135c1d464e1b35290130c131a4e5639
Benedito <75847009+MarcosBenedito@users.noreply.github.com>
2021-02-11 11:36:57 -0300

    Update makeCsv.py

--- NFLproject/makeCsv.py
+++ NFLproject/makeCsv.py
@@ -4,23 +4,23 @@ import MySQLdb
 import csv
 
 def makeCsv(url):
-    
+   
     html = urlopen(url)
     bsObj = BeautifulSoup(html,"html.parser")
-
     divFather = bsObj.find("div",{"class":"team-content"})
     divSon = divFather.find("div",{"id":"dataWrapper"})
     body = divSon.find("tbody")
     trs = body.findAll("tr")
     names_links ={}
+    
     for player in range(len(trs)):
         anchor = trs[player].find("a",{"class":"team-name"})
 
         if 'href' in anchor.attrs:
             names_links[anchor.get_text()] = anchor.attrs['href'] 
             
-    with open('dict.csv','w') as f:
+    with open('dictNBA.csv','w') as f:
         w = csv.writer(f)
         for key, value in names_links.items():
             w.writerow([key, value])
-#makeCsv("https://www.spotrac.com/nfl/contracts/sort-value/limit-2000/")
+makeCsv("https://www.spotrac.com/nba/contracts/sort-value/limit-2000/")
