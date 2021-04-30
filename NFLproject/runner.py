f51c520e68494c1d096ee37d0ce9fd7a1be80843
Marcos Benedito <75847009+BeneditoDev@users.noreply.github.com>
2021-04-30 20:16:16 -0300

    Create runner.py

---
+++ NFLproject/runner.py
@@ -0,0 +1,53 @@
+from getInfo import getInfoNFL
+from makeCsv import makeCsv
+from getInfo import getInfoNFL
+from makeHash import makeHash, veriHash
+from createDb import createDB
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import MySQLdb
+import csv
+
+def printMenu():
+    print("")
+    print("================================================================================================")
+    print("    To work correctly you need to create a database with a valid user before run option 2       ")
+    print("================================================================================================")
+    print("")
+    print("[1] | Make Csv.")
+    print("[2] | Make a new base or continue an existing one.")
+    choice = input("Answer:")
+    if choice != '1' and choice != '2':
+        printMenu()
+    else:
+        return choice
+
+
+def op1():
+    makeCsv("https://www.spotrac.com/nfl/contracts/sort-value/limit-2000/")
+
+def op2():
+    print("Configure a valid connection to an existing database")
+    mydb = input("Choose the database to be used (The function wil handle if is a new database or no): ")
+    myhost = input("Mysql host: ")
+    myuser = input("Mysql user: ")
+    mypass = input("Mysql password: ")
+    try:
+        createDB(myhost, myuser, mypass, mydb)
+    except:
+        print("  ")
+        print("Host, user, password, or database invalid.")
+        print("  ")
+
+def executeMenu():
+
+    option = printMenu()
+
+    if option == "1":
+        op1()
+        executeMenu()
+    elif option == "2":
+        op2()
+        executeMenu()
+
+executeMenu()
